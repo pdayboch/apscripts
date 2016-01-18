7d156cddf27e66b49c5df045e298cb332f744fe8
Philip <philip245@gmail.com>
2016-01-18 12:03:08 -0800

    added multi threading
    added multi threading
    added MAC to link local ipv6 auto conversion

--- lib/apscripts.rb
+++ lib/apscripts.rb
@@ -1,43 +1,63 @@
 require './lib/apscripts/ssh.rb'
+require 'tilt/erb'
 
 class Setup
   attr_accessor :aps
 
   def initialize(rawIPs)
     @aps = Hash.new
-
     ips = parseIps(rawIPs)
 
-    ips.each do |ip|
-      @aps[ip.chomp] = Hash.new
-    end
-
-    getConfig
-    parseConfig("product_model", :model)
-    parseConfig("node_name", :name)
+    getData
   end
 
   private
   def parseIps(rawIPs)
     ips = rawIPs.split("\n")
+    ips.each do |ip|
+      if !ip.match(/^fd0a:9b09:1f7/)
+        m = ip.split(":")
+        hex = m[0].to_i(16)
+        flip = hex ^ 0x02
+        flippedMac = "#{flip.to_s(16)}#{m[1]}:#{m[2]}ff:fe#{m[3]}:#{m[4]}#{m[5]}"
+        ipv6 = "fd0a:9b09:1f7:0:#{flippedMac}"
+        ip = ipv6
+      end
+      @aps[ip.chomp] = Hash.new
+    end
     return ips
   end
 
   private
-  def getConfig()
-    cmd = "cat /storage/config"
+  def getData()
+    threads = []
     @aps.each do |ip, info|
-      config = Ssh.do_ssh(ip, cmd)
-      @aps[ip][:config] = config
+      threads.push(Thread.new{
+        getConfig(ip)
+        parseConfig(ip, "product_model", :model)
+        parseConfig(ip, "node_name", :name)
+      })
+      threads.each {|t| t.join}
     end
   end
 
   private
-  def parseConfig(lineText, hashId)
-    @aps.each do |ip, info|
-      foundline = info[:config].match(/^#{lineText}.*/)[0]
+  def getConfig(ip)
+    cmd = "cat /storage/config"
+    config = Ssh.do_ssh(ip, cmd)
+    @aps[ip][:config] = config
+  end
+
+  private
+  def parseConfig(ip, lineText, hashId)
+    info = @aps[ip]
+    result = info[:config].match(/^#{lineText}.*/)
+    if result
+      foundline = result[0]
       des = foundline.match(/\s.*/)[0].strip
       info[hashId] = des
+    else
+      info[hashId] = "Not Found"
     end
   end
 
